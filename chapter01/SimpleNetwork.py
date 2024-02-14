5225cab504ba4f874006098b046b36d35b6a1f9e
luis <alvarez.en.luis@gmail.com>
2024-02-13 22:26:01 -0800

    still working on the simplenetwork

--- chapter01/SimpleNetwork.py
+++ chapter01/SimpleNetwork.py
@@ -10,9 +10,18 @@ class SimpleNetwork(object):
   """
   A simple fully-connected NN.
   Args:
-    numInputs ()
-    numOutputs ()
-    hiddenLayersSizes ()
+    numInputs (int): The input vector size / number of input values.
+    numOutputs (int): The output vector size
+    hiddenLayersSizes (list): A list of sizes for each hidden layer to be added to the network
   Attributes:
-
+    layers (list): The list of layers forming this simple network
   """
+  def __init__(self, numInputs, numOutputs, hiddenLayersSizes=(64,32)):
+    super().__init__()
+    # We build the list of layers composing the network:
+    sizes = [numInputs, *hiddenLayersSizes, numOutputs]
+    self.layers = [
+      FullyConnectedLayer(sizes[i], sizes[i+1], sigmoid)
+      for i in range(len(sizes) - 1)
+    ]
+    
